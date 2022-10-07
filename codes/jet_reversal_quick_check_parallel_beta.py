0cf71ac36acb03773fb0c6e6b4f9ba5a790ad86d
Ramiz Qudsi <qudsiramiz@gmail.com>
2022-10-07 13:27:32 -0400

    modified:   ../codes/jet_reversal_quick_check_parallel_beta.py 	deleted:    mms_jet_reversal_check_error_log_20221007_beta.csv 	deleted:    mms_jet_reversal_times_list_20221007_beta.csv 	deleted:    ../figures/jet_reversal_checks_beta/jet/mms3_jet_reversal_check_20150904_114254_lmn_rolling_median.png

--- codes/jet_reversal_quick_check_parallel_beta.py
+++ codes/jet_reversal_quick_check_parallel_beta.py
@@ -29,7 +29,7 @@ def check_jet_reversal(crossing_time):
     crossing_time = crossing_time.replace(tzinfo=pytz.utc)
     # Try with 'brst' data rate, if that fails then try with 'fast'
     inputs = {'crossing_time': crossing_time,
-              'dt': 300,
+              'dt': 120,
               'probe': 3,
               'jet_len': 3,
               'level': 'l2',
