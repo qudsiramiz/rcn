140bd6b869df662c23434a2562c570ed85e4c1c4
Ramiz Qudsi <qudsiramiz@gmail.com>
2022-10-18 16:20:08 -0400

    modified:   rx_code.py 	new file:   ../data/.~lock.mms_jet_reversal_times_list_20221017_beta.csv# 	new file:   ../data/rx_d/reconnection_line_data_mms3_20221018.csv 	new file:   ../figures/all_ridge_plots/t96/None_interpolation_mms3/v11/ridge_plot_2015-09-02_16-40-20_2015-09-02_16-50-20_-2_3_1.png 	new file:   ../figures/all_ridge_plots/t96/None_interpolation_mms3/v11/ridge_plot_2015-09-02_17-48-27_2015-09-02_17-58-27_-5_1_-4.png 	new file:   ../figures/all_ridge_plots/t96/None_interpolation_mms3/v11/ridge_plot_2015-09-03_14-07-42_2015-09-03_14-17-42_1_5_-0.png

--- codes/rx_code.py
+++ codes/rx_code.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# from rx_model_funcs import rx_model, ridge_finder_multiple
 import rx_model_funcs as rmf
 
 importlib.reload(rmf)
@@ -23,61 +22,7 @@ start = time.time()
 
 today_date = datetime.datetime.today().strftime("%Y-%m-%d")
 
-# trange_list = [
-# ['2016-12-24 15:08:00', '2016-12-24 15:12:00'],
-# ['2016-12-07 05:11:00', '2016-12-07 05:21:00'],
-# #['2015-09-08 11:05:00', '2015-09-08 11:15:00'],
-# #['2015-09-19 07:43:30'],
-# ['2015-10-16 10:33:30'],
-# ['2015-10-16 13:07:02'],
-# ['2015-10-22 06:05:22'],
-# ['2015-11-01 15:08:06'],
-# ['2015-11-12 07:19:21'],
-# ['2015-12-06 23:38:31'],
-# ['2015-12-08 11:20:44'],
-# ['2015-12-09 01:06:11'],
-# ['2015-12-14 01:17:40'],
-# ['2016-01-07 09:36:15'],
-# ['2016-01-10 09:13:37'],
-# ['2016-10-22 12:58:41'],
-# ['2016-11-02 14:46:18'],
-# ['2016-11-06 08:40:58'],
-# ['2016-11-12 17:48:47'],
-# ['2016-11-13 09:10:41'],
-# ['2016-11-18 12:08:11'],
-# #['2016-11-23 07:49:33'],
-# #['2016-11-23 07:49:52'],
-# ['2016-11-23 07:50:30'],
-# ['2016-11-28 15:47:00'],
-# ['2016-12-11 04:41:50'],
-# ['2016-12-19 14:15:02'],
-# ['2017-01-02 02:58:13'],
-# ['2017-01-11 04:22:43'],
-# ['2017-01-20 12:32:07'],
-# ['2017-01-22 10:15:46'],
-# #['2017-01-22 10:15:58'],
-# ['2017-01-22 10:47:33'],
-# ['2017-01-27 12:05:43'],
-# ['2015-09-25 12:05:00', '2015-09-25 12:13:00'],
-# ['2015-09-11 15:23:00'],
-# ['2016-01-17 06:29:40'],
-# ['2015-09-19 07:41:38'],
-# ['2015-09-18 11:54:02', '2015-09-18 11:55:59']
-# ]
-
-# trange_list = [['2015-09-11 15:18:00', '2015-09-11 15:28:00']]
-
-# trange_list = [['2017-01-22 10:47:33']]
-# trange_list = [['2016-12-24 15:08:00', '2016-12-24 15:12:00']]
-# trange_list = [['2017-01-22 10:47:33'],
-#                #['2015-12-06 23:38:31'],
-#                ['2016-12-24 15:08:00', '2016-12-24 15:12:00']
-#                ]
-#  Sort the trange_list by the start time
-# trange_list.sort(key=lambda x: x[0])
-# trange_ind_list = np.array([10]) #, 70, 75, 104, 2263, 2259, 2257, 2142, 2052, 2053])
-
-df_jet_reversal = pd.read_csv("../data/mms_jet_reversal_times_list_20221007_beta.csv",
+df_jet_reversal = pd.read_csv("../data/mms_jet_reversal_times_list_20221017_beta.csv",
                               index_col=False)
 
 # If nay column has NaN, drop that row
@@ -96,7 +41,7 @@ trange_list = df_jet_reversal.index.tolist()
 # trange_list_new = trange_list[trange_ind_list]
 mms_probe_num_list = [1, 2, 3, 4]
 ind_min = 0
-ind_max = -1
+ind_max = 10
 
 
 @contextmanager
@@ -197,13 +142,13 @@ for mms_probe_num in mms_probe_num_list[2:3]:
                     "interpolation": "None",
                     "tsy_model": model_type,
                     "dark_mode": True,
-                    "rc_file_name": f"reconnection_line_data_mms{mms_probe_num}_20221007.csv",
+                    "rc_file_name": f"reconnection_line_data_mms{mms_probe_num}_20221018.csv",
                     "rc_folder": "../data/rx_d/",
                     "save_rc_file": True,
                     "walen1": df_jet_reversal["walen1"][ind_range],
                     "walen2": df_jet_reversal["walen2"][ind_range],
                     "jet_detection": df_jet_reversal["jet_detection"][ind_range],
-                    "fig_version": "v10",
+                    "fig_version": "v11",
                     "r_W": df_jet_reversal["r_W"][ind_range],
                     "theta_W": df_jet_reversal["theta_w"][ind_range],
                     # "jet_time": df_jet_reversal["jet_time"][ind_range],
